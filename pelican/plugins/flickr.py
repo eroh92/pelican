a900731c708a3d99548977a967e28d3fc7a7722e
Rob Eroh <rob@eroh.me>
2013-02-06 08:49:04 -0800

    flickr plugin to add photos from a flickr photoset to a post

---
+++ pelican/plugins/flickr.py
@@ -0,0 +1,90 @@
+import flickr_api
+
+from datetime import datetime
+from pelican import signals
+
+"""
+Flickr plugin for pelican
+================================
+
+Adds flickr set metadata to article objects.
+
+Requirements
+------------
+    pip install flickr-api
+
+Settings
+--------
+To enable, add
+
+    from pelican.plugins import flickr
+    PLUGINS = [flickr]
+
+    FLICKR_KEY = 'XXXXX'
+    FLICKR_SECRET = 'XXXXX'
+
+to your settings.py.
+
+To control the attributes of the photo objects, add the following
+   
+    FLICKR_EXTRA_PARAMS = {'extras': 'date_taken, geo, tags, o_dims, url_l'}
+
+to your settings.py.
+
+Add a flickr photoset id in the metadata of your article file:
+
+    Flickrset: XXXX
+
+If you leave out Date and Title from the article metadata, this plugin pulls
+the title and date from the photoset.
+
+Usage
+-----
+    <h1>Photo Album: {{ article.photoset.title }}</h1>
+    <ul>
+    {% for photo in article.photos %}
+        <img height="{{ photo.height_l }}"
+             width="{{ photo.width_l }}" 
+             src="{{ photo.url_l }}"
+             alt="{{ photo.title }}"/>
+        <span>{{ photo.title }}</span>
+    {% endfor %}
+    </ul>
+
+"""
+
+def has_flickr_settings(generator):
+    return generator.settings.has_key('FLICKR_SECRET') and \
+            generator.settings.has_key('FLICKR_KEY')
+
+
+def initialize_flickr_api(generator):
+    if has_flickr_settings(generator):
+        flickr_api.set_keys(generator.settings['FLICKR_KEY'],
+                            generator.settings['FLICKR_SECRET'])
+        if not generator.settings.has_key('FLICKR_EXTRA_PARAMS'):
+            generator.settings['FLICKR_EXTRA_PARAMS'] = \
+                {'extras': 'url_l, url_m'}
+            
+
+def add_flickr_metadata(generator, metadata):
+    if has_flickr_settings(generator) and metadata.has_key('flickrset'):
+        photoset = flickr_api.Photoset(id=metadata['flickrset'])
+        photoset.load()
+        metadata['photoset'] = photoset
+        metadata['photos'] = \
+            photoset.getPhotos(**generator.settings['FLICKR_EXTRA_PARAMS'])
+        if not metadata.has_key('title'):
+            metadata['title'] = photoset.title
+        if not metadata.has_key('thumbnail'):
+            for photo in metadata['photos']:
+                if photo.isprimary == '1':
+                    metadata['thumbnail'] = photo.url_l
+        if not metadata.has_key('date'):
+            metadata['date'] = \
+                datetime.fromtimestamp(float(photoset.date_create))
+
+
+def register():
+    signals.article_generator_init.connect(initialize_flickr_api)
+    signals.article_generate_context.connect(add_flickr_metadata)

d3a36d9d4d9c55bc93f981f64f6f9e569f971de0
Rob Eroh <rob@eroh.me>
2013-03-25 00:06:45 -0800

    locations stable

--- pelican/plugins/post_locations.py
+++ pelican/plugins/post_locations.py
@@ -8,49 +8,96 @@ from pelican import signals, contents
 from pelican.utils import slugify
 
 
-class Country(contents.URLWrapper):
+class Location(contents.URLWrapper):
     def __init__(self, name, *args, **kwargs):
-        name = name.strip()
-        super(Country, self).__init__(name, *args, **kwargs)
+        locations = [loc.strip() for loc in name.split(',')]
+        super(Location, self).__init__(locations.pop(0), *args, **kwargs)
+        if len(locations) > 0:
+            self.parent = make_location(', '.join(locations), child=self, *args, **kwargs)
+        self.slug = self._location_slug
+        self.children = {}
 
+    def __getitem__(self, key):
+        return self.children[key]
 
-class City(contents.URLWrapper):
-    def __init__(self, name, *args, **kwargs):
-        name = name.strip()
-        city, country = name.split(',')
-        self.city = city.strip()
-        self.country = Country(country, *args, **kwargs)
-        super(City, self).__init__(name, *args, **kwargs)
-        self.slug = "%s/%s" % (slugify(self.city), slugify(self.country))
+    def __setitem__(self, key, value):
+        self.children[key] = value
+
+    @property
+    def has_parent(self):
+        return hasattr(self, 'parent')
+
+    @property
+    def _location_slug(self):
+        if self.has_parent:
+            return '%s/%s' % (self.parent._location_slug, slugify(self.name))
+        else:
+            return slugify(self.name)
+
+    @property
+    def full_name(self):
+        if self.has_parent:
+            return '%s, %s' % (self.name, self.parent.full_name)
+        else:
+            return self.name
+
+    @property
+    def all_location_names(self):
+        return [loc.name for loc in self.all_locations]
+
+    @property
+    def all_locations(self):
+        if self.has_parent:
+            locations = self.parent.all_locations
+            locations.append(self)
+            return locations
+        else:
+            return [self]
+
+def _format_name(name):
+    return ', '.join([loc.strip() for loc in name.split(',')])
+
+location_storage = {}
+def make_location(name, child=None, *args, **kwargs):
+    name = _format_name(name)
+    if not location_storage.has_key(name):
+        location = Location(name, *args, **kwargs)
+        location_storage[name] = location
+    location = location_storage[name]
+    if child:
+        location[child.name] = child
+    return location
 
 
 def init_locations(article_generator):
-    article_generator.locations = defaultdict(list)
-    article_generator.countries = \
-        defaultdict(article_generator.locations.__copy__)
+    article_generator.locations = defaultdict(set)
+    article_generator.location_hierarchy = {}
 
 
 def parse_metadata(article_generator, metadata):
     if metadata.has_key('locations'):
         locations = []
         for location in metadata['locations'].split(';'):
-            locations.append(City(location,
-                                  settings=article_generator.settings))
+            locations.append(make_location(location,
+                                           settings=article_generator.settings))
         metadata['locations'] = locations
 
 
 def add_locations_to_generator(article_generator):
     for article in article_generator.articles:
         if article.status == "published" and hasattr(article, 'locations'):
-            for location in article.locations:
-                article_generator.locations[location].append(article)
-                article_generator.countries[location.country][location].append(article)
+            for locations in article.locations:
+                for location in locations.all_locations:
+                    article_generator.locations[location].add(article)
+                    if not location.has_parent:
+                        article_generator.location_hierarchy[location.name] = \
+                            location
 
 
 def generate_locations_pages(article_generator, write):
     location_template = article_generator.get_template('location')
     for location, articles in article_generator.locations.items():
-        articles.sort(key=attrgetter('date'), reverse=True)
+        articles = sorted(articles, key=attrgetter('date'), reverse=True)
         dates = [article for article in article_generator.dates
                  if article in articles]
         write(location.save_as,

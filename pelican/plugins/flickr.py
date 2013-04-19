9293ae20bcdc4a07bcea6832af35cfdcfee6ae21
Rob Eroh <rob@eroh.me>
2013-04-19 10:21:57 +0700

    flickr support for single image inserts

--- pelican/plugins/flickr.py
+++ pelican/plugins/flickr.py
@@ -1,6 +1,9 @@
+import re
+
 import flickr_api
 
 from datetime import datetime
+from functools import partial
 from pelican import signals
 
 from memorised.decorators import memorise
@@ -55,6 +58,16 @@ Usage
 
 """
 
+photo_cache = {}
+
+def _get_photo(id, extra_params):
+    print 'lookup photo %s' % id
+    id = unicode(id)
+    if not photo_cache.has_key(id):
+        photo = flickr_api.Photo(id=id, **extra_params)
+        photo_cache[id] = photo
+    return photo_cache[str(id)]
+
 def has_flickr_settings(generator):
     return generator.settings.has_key('FLICKR_SECRET') and \
             generator.settings.has_key('FLICKR_KEY')
@@ -67,12 +80,22 @@ def initialize_flickr_api(generator):
         if not generator.settings.has_key('FLICKR_EXTRA_PARAMS'):
             generator.settings['FLICKR_EXTRA_PARAMS'] = \
                 {'extras': 'url_l, url_m'}
+        if not generator.settings.has_key('FLICKR_MAX_HEIGHT'):
+            generator.settings['FLICKR_MAX_HEIGHT'] = 768
+        if not generator.settings.has_key('FLICKR_MAX_WIDTH'):
+            generator.settings['FLICKR_MAX_WIDTH'] = 1024
+        if not generator.settings.has_key('FLICKR_THUMBNAIL_MAX_HEIGHT'):
+            generator.settings['FLICKR_THUMBNAIL_MAX_HEIGHT'] = 480
+        if not generator.settings.has_key('FLICKR_THUMBNAIL_MAX_WIDTH'):
+            generator.settings['FLICKR_THUMBNAIL_MAX_WIDTH'] = 640
 
 
 def _get_photoset_data(photoset_id, extra_params):
     photoset = flickr_api.Photoset(id=photoset_id)
     photoset.load()
     photos = photoset.getPhotos(**extra_params)
+    for photo in photos:
+        photo_cache[photo.id] = photo
     return (photoset, photos)
 
 
@@ -94,16 +117,72 @@ def add_flickr_metadata(generator, metadata):
                 datetime.fromtimestamp(float(photoset.date_create))
 
 
+flickr_re = re.compile(r"[<p>]*\|flickr:(?P<flickr_id>.*?)\|[</p>]*")
+
+def _get_url(photo, max_height, max_width):
+    for size, data in sorted(photo.getSizes().items(),
+                             key=lambda x: int(x[1]['width']),
+                             reverse=True):
+        url = data['source']
+        height = int(data['height'])
+        width = int(data['width'])
+
+        if (height <= max_height and
+            width <= max_width):
+            return url
+
+def thumbnail_replace(photo, generator, *args, **kwargs):
+    photo_url = _get_url(photo,
+                         generator.settings['FLICKR_THUMBNAIL_MAX_HEIGHT'],
+                         generator.settings['FLICKR_THUMBNAIL_MAX_WIDTH'])
+    return photo_url
+
+def insert_image(photo, article, generator, *args, **kwargs):
+    template = generator.get_template('flickr_image')
+    photo_url = _get_url(photo,
+                         generator.settings['FLICKR_MAX_HEIGHT'],
+                         generator.settings['FLICKR_MAX_WIDTH'])
+    return template.render(photo=photo,
+                           photo_url=photo_url,
+                           article=article,
+                           **generator.settings)
+
+def flickr_replace(generator, article, replacer_func):
+    def replacer(m, generator, article, replacer_func):
+        photo = _get_photo(m.group('flickr_id'),
+                           generator.settings['FLICKR_EXTRA_PARAMS'])
+        return replacer_func(photo=photo,
+                             article=article,
+                             generator=generator)
+    return partial(replacer,
+                   generator=generator,
+                   article=article,
+                   replacer_func=replacer_func)
+
 def article_update(generator, article):
     flickr_var = '|flickr_images|'  
-  
     if (has_flickr_settings(generator)
             and hasattr(article, 'flickrset')
             and article._content.find(flickr_var) > -1):
-        template = generator.get_template('flickr')
+        template = generator.get_template('flickr_images')
         output = template.render(article=article, **article.settings)
         article._content = article._content.replace(flickr_var, output)
         article.get_content.func.im_self.cache.clear()
+    if has_flickr_settings(generator):
+        if hasattr(article, 'thumbnail'):
+            thumbnail = \
+                flickr_re.sub(flickr_replace(generator,
+                                             article,
+                                             thumbnail_replace),
+                              article.thumbnail)
+            article.thumbnail = thumbnail
+            article.metadata['thumbnail'] = thumbnail
+        new_content = \
+            flickr_re.sub(flickr_replace(generator, article, insert_image),
+                          article._content)
+        if new_content is not article._content:
+            article._content = new_content
+            article.get_content.func.im_self.cache.clear()
 
 
 def register():
